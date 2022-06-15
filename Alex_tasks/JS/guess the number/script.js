74b157214e4f88a74278849f0fa22453f9660b0b
LordMalacath <104316712+LordMalacath@users.noreply.github.com>
2022-06-15 15:11:55 +0300

    guessNumberDone

--- Alex_tasks/JS/guess the number/script.js
+++ Alex_tasks/JS/guess the number/script.js
@@ -4,28 +4,41 @@ function randomInteger(min = 1, max = 20) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 };
-let number = randomInteger();
-console.log(number);
-let score = Number(document.querySelector('.score').innerHTML);
-let highScore = Number(document.querySelector('.highscore').innerHTML);
+
 const message = document.querySelector('.message');
 const checkBtn = document.querySelector('.check');
-const body = document.body;
+let startScore = document.querySelector('.score');
+const initialScore = +startScore.textContent;
+let highScore = document.querySelector('.highscore');
+const data = {
+    guessNumber: randomInteger(),
+    score:  Number(startScore.textContent),
+};
+console.log(data.guessNumber);
+const guessInput = document.getElementById('guessInput');
 checkBtn.addEventListener('click', () => {
-    const guessInput = document.querySelector('.guess').value;
-    if (number > guessInput)
-        [
-            message.innerHTML = 'Too low!',
-            score -= 1,
-            console.log(score)
-        ];
-    else if (number == guessInput)
-        [
-            message.innerHTML = 'Match!!!',
-            body.style.background = "#60b347"];
-    else
-        [
-            message.innerHTML = 'Too hight!']
+    startScore.textContent = --data.score;
+    console.log(data.score);
+    if (data.guessNumber > guessInput.value) {
+        message.innerHTML = 'Too low!';
+    }
+    else if (data.guessNumber == guessInput.value) {
+        message.innerHTML = 'Match!!!';
+        document.body.style.background = "#60b347";
+        highScore.textContent = data.score;
+    }
+    else {
+        message.innerHTML = 'Too hight!';
+    }
 });
-// const refreshBtn = document.querySelector('.again');
-// refreshBtn.addEventListener('click', randomInteger);
+
+
+const refreshBtn = document.querySelector('.again');
+refreshBtn.addEventListener('click', () => {
+    startScore.textContent = initialScore;
+    document.body.style.background = "";
+    data.guessNumber = randomInteger();
+    console.log(data.guessNumber);
+});
+
+
