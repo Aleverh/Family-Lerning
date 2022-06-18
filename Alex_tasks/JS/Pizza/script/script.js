b6efc8bb67dcb71d187f3dc880670f1ab0fb6d32
LordMalacath <104316712+LordMalacath@users.noreply.github.com>
2022-06-18 16:49:59 +0300

    pizza

--- Alex_tasks/JS/Pizza/script/script.js
+++ Alex_tasks/JS/Pizza/script/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const showBtn = document.querySelectorAll('.show-modal');
-const closeBtn = document.querySelectorAll('.close-modal');
+const showBtn = document.querySelectorAll('.header__basket');
+const closeBtn = document.querySelectorAll('.modal__close');
+const choseBtn = document.querySelectorAll('.size');
+const addBtn = document.querySelectorAll('.card__button');
 
 const modal = [document.querySelector('.modal'), document.querySelector('.overlay')];
 
@@ -13,25 +15,27 @@ showBtn.forEach(element => {
         });
     });
 });
+
 //closing modal function
 function closeModal() {
     modal.forEach(item => {
         item.classList.add('hidden')
     })
 };
+
 //close modal by pressing Esc
 document.addEventListener('keyup', function (e) {
     if (e.code === 'Escape') {
         closeModal()
     }
 });
+
 //closing modal by pressing btn
 closeBtn.forEach(element => {
     element.addEventListener('click', closeModal);
 });
-//closing modal by clicking outside modal
-// modal[1].addEventListener('click', closeModal);
 
+//closing modal by clicking outside modal
 document.addEventListener('click', function (e) {
     if (!e.target.closest('modal') && !e.target.closest('.show-modal')) {
         closeModal()
