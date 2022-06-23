02da97d3408fab06475921a212c9b10df8f63a20
LordMalacath <104316712+LordMalacath@users.noreply.github.com>
2022-06-23 16:24:14 +0300

    pizzaUpp

--- Alex_tasks/JS/Pizza/script/script.js
+++ Alex_tasks/JS/Pizza/script/script.js
@@ -46,43 +46,54 @@ closeBtn.forEach(element => {
 
 /* addBtn.addEventListener('click', () => {
     modal[0].insertAdjacentHTML("beforeEnd", '<div class="addedItem"><img src="./img/pizza1.png" alt="" class="addedItem__img"><span class="addedItem__name">Pizza#1</span><img src="./img/minusbutton.png" alt="" class="addedItem__minusButton"><img src="./img/plusbutton.png" alt="" class="addedItem__plusButton">span class="addedItem__amount">0</span><span class="addedItem__cost">0,00$</span></div>')
-});
- */
-const pizzaNames = document.querySelectorAll('.card__title');
-pizzaNames.forEach(element => {
-    element.textContent
+}); */
+
+/* function getData(item, name) {
+    const pizzaItemCollection = document.querySelectorAll('item');
+    const pizzaItemData = Array.from(pizzaItemCollection);
+    return {const pizzaName = }
+} */
+let pizzaNamesData = document.querySelectorAll('.card__title');
+pizzaNamesData = Array.from(pizzaNamesData);
+const pizzaNames = pizzaNamesData.map((name) => {
+    return name.textContent;
+})
+
+let pizzaPriceData = document.querySelectorAll('.price');
+pizzaPriceData = Array.from(pizzaPriceData);
+const pizzaPrices = pizzaPriceData.map((price) => {
+    return price.textContent;
 });
 
-const pizzaPrices = document.querySelectorAll('.price');
-pizzaPrices.forEach(element => {
-    element.textContent
-});
+let pizzaImgsData = document.querySelectorAll('.card__img');
+pizzaImgsData = Array.from(pizzaImgsData);
+const pizzaImgs = pizzaImgsData.map((img) => {
+    return img.innerHTML;
+})
+console.log(pizzaNames,pizzaPrices,pizzaImgs);
 
-const pizzaImgs = document.querySelectorAll('.card__img');
-pizzaImgs.forEach(element => {
-    element.innerHTML
-});
-
-// console.log(pizzaNames,pizzaPrices,pizzaImgs);
 const pizza1 = {
-    name:"",
-    price:"",
-    img:"",
+    name:pizzaNames[0],
+    price:pizzaPrices[0],
+    img:pizzaImgs[0],
 };
+console.log(pizza1);
 const pizza2 = {
-    name:"",
-    price:"",
-    img:"",
+    name:pizzaNames[1],
+    price:pizzaPrices[1],
+    img:pizzaImgs[1],
 };
 const pizza3 = {
-    name:"",
-    price:"",
-    img:"",
+    name:pizzaNames[2],
+    price:pizzaPrices[2],
+    img:pizzaImgs[2],
 };
 const pizza4 = {
-    name:"",
-    price:"",
-    img:"",
+    name:pizzaNames[3],
+    price:pizzaPrices[3],
+    img:pizzaImgs[3],
 };
+
+
 const pizzaData = [pizza1,pizza2,pizza3,pizza4];
-// console.log(pizzaData);
+console.log(pizzaData);
