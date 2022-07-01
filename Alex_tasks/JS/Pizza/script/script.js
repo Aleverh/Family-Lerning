2bbc96e4e3039ccb4189541171b66b31fe0c8c48
LordMalacath <104316712+LordMalacath@users.noreply.github.com>
2022-07-01 16:02:22 +0300

    Pizza localStorage add

--- Alex_tasks/JS/Pizza/script/script.js
+++ Alex_tasks/JS/Pizza/script/script.js
@@ -1,171 +1,172 @@
 'use strict';
 
-const showBtn = document.querySelectorAll('.header__basket');
-const closeBtn = document.querySelectorAll('.modal__close');
+const showBtn = document.querySelector('.header__basket');
 const modal = [document.querySelector('.modal'), document.querySelector('.overlay')];
 
-showBtn.forEach(element => {
-    element.addEventListener('click', function (e) {
-        e.preventDefault();
-        modal.forEach(item => {
-            item.classList.remove('hidden')
-        });
+showBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    modal.forEach(item => {
+        item.classList.remove('hidden')
     });
 });
-
-//-------------------closing modal function
+//?----------------------------------------------------
+/* document.addEventListener('click', function (e) {
+    if (e.target.closest('.basket')) {
+        openModal();
+    }
+}); */
+//?----------------------------------------------------
+//-------------------open modal function--------------------------------------
+function openModal() {
+    modal.forEach(item => {
+        item.classList.remove('.hidden');
+    });
+}
+//-------------------closing modal function--------------------------------------
 function closeModal() {
     modal.forEach(item => {
         item.classList.add('hidden')
     })
 };
-
-//------------------close modal by pressing Esc
+//------------------close modal by pressing Esc----------------------------------
 document.addEventListener('keyup', function (e) {
     if (e.code === 'Escape') {
         closeModal()
     }
 });
-
-//----------------closing modal by pressing btn
-closeBtn.forEach(element => {
-    element.addEventListener('click', closeModal);
-});
-
-//---------------------closing modal by clicking outside modal
-/* document.addEventListener('click', function (e) {
-     const isModalOpen = modal[0].classList.contains('hidden');
-    if (isModalOpen && !e.target.closest('modal') && !e.target.closest('.show-modal')) {
+//----------------closing modal by pressing btn----------------------------------
+document.addEventListener('click', function (e) {
+    if (e.target.closest('.close')) {
         closeModal()
     }
-    console.log(isModalOpen);
-}); */
-
-//-------------function for gettin array with data
-/* function getData(item) {
-    let pizzaItemCollection = document.querySelectorAll(item);
-    pizzaItemCollection = Array.from(pizzaItemCollection);
-    const pizzaItem = pizzaItemCollection.map((name => {
-        return name.textContent;
-    }))
-    return {pizzaItem}
-}
-
-const test = getData('.card__title');
-console.log(test); */
-
-
-const pizzaNamesData = Array.from(document.querySelectorAll('.card__title'));
-const pizzaNames = pizzaNamesData.map((name) => {
-    return name.textContent;
-})
-
-const pizzaPriceData = Array.from(document.querySelectorAll('.price'));
-const pizzaPrices = pizzaPriceData.map((price) => {
-    return price.textContent;
 });
-
-const pizzaImgsData = Array.from(document.querySelectorAll('.card__img'));
-const pizzaImgs = pizzaImgsData.map((img) => {
-    return img.innerHTML;
+//----------------closing modal by clicking outside modal-------------------------
+document.addEventListener('click', function (e) {
+    if (e.target === modal[1]) {
+        closeModal();
+    }
 });
-
+//----------------pizza data------------------------------------------------------
 const pizza1 = {
     id: 1,
-    name: pizzaNames[0],
-    price: pizzaPrices[0],
-    img: pizzaImgs[0],
+    name: 'Верона',
+    price: 110.00,
+    description: `Шинка, гриби печериці, сир пармезан, трюфельна олія, базилік, сир
+    моцарелла, вершковий соус`,
+    size: 40,
+    img: `<img src="./img/pizza1.png" alt="">`,
 };
 const pizza2 = {
     id: 2,
-    name: pizzaNames[1],
-    price: pizzaPrices[1],
-    img: pizzaImgs[1],
+    name: 'Діавола',
+    price: 135.00,
+    description: `Салямі чорізо, салямі мілано, перець чилі свіжий і сухий, цибуля
+    маринована, сир моцарелла, неаполітанський соус`,
+    size: 40,
+    img: `<img src="./img/pizza2.png" alt="">`,
 };
 const pizza3 = {
     id: 3,
-    name: pizzaNames[2],
-    price: pizzaPrices[2],
-    img: pizzaImgs[2],
+    name: 'Карбонара',
+    price: 120.00,
+    description: `Бекон, жовток курячий, часник, базилік, свіжий сир моцарелла, сир пармезан,
+    вершковий соус`,
+    size: 40,
+    img: `<img src="./img/pizza3.png" alt="">`,
 };
 const pizza4 = {
     id: 4,
-    name: pizzaNames[3],
-    price: pizzaPrices[3],
-    img: pizzaImgs[3],
+    name: 'Пепероні',
+    price: 125.00,
+    description: `Салямі перероні, сир моцарела, сир пармезан, спеції, томатний соус`,
+    size: 40,
+    img: `<img src="./img/pizza4.png" alt="">`,
 };
 
-//------------------ arr of all objects
 const pizzaData = [pizza1, pizza2, pizza3, pizza4];
 
-//------------------ adding element into basket
-function addChosenPizza(pizzaData) {
+//------------------set data into html--------------------------------------
+const itemCards = Array.from(document.querySelectorAll('.menu__card'));
+itemCards.forEach(element => {
+    const elementParrent = element.closest('.menu__card');
+    const ourPizza = pizzaData.find(item => item.id === Number(elementParrent.getAttribute('data-id')));
+    const name = elementParrent.querySelector('.card__title');
+    const price = elementParrent.querySelector('.price');
+    const description = elementParrent.querySelector('.card__description');
+    const size = elementParrent.querySelector('.size');
+    const img = elementParrent.querySelector('.card__img');
+    name.innerHTML = ourPizza.name;
+    price.innerHTML = ourPizza.price + ' грн';
+    description.innerHTML = ourPizza.description;
+    size.innerHTML = ourPizza.size + ' см';
+    img.innerHTML = ourPizza.img;
+});
+//------------------ adding element function and button------------------------------
+function addFoundPizza(myPizza) {
     modal[0].insertAdjacentHTML("beforeEnd",
         `
-    <div class="addedItem">
-    <span class="addedItem__remove">&times;</span>
-    <div class="addedItem__img">${pizzaData.img}</div>
-    <span class="addedItem__name">${pizzaData.name}</span>
-    <img src="./img/minusbutton.png" alt="" class="addedItem__minusButton">
-    <img src="./img/plusbutton.png" alt="" class="addedItem__plusButton">
-    <span class="addedItem__amount">1</span>
-    <span class="addedItem__cost">${pizzaData.price}</span>
-    </div>
-`);
-    // -------------remove added pizza
-    let deleteItem = document.getElementsByClassName('addedItem__remove');
-    deleteItem = Array.from(deleteItem);
-    deleteItem.forEach(item => {
-        item.addEventListener("click", function () {
-            item.parentElement.remove();
-        });
-    });
-    //---------edit amount of pizza-----------
-    const pizzaPlus = Array.from(document.getElementsByClassName('addedItem__plusButton'));
-    const pizzaMinus = Array.from(document.getElementsByClassName('addedItem__minusButton'));
-    const curentPizzaAmount = document.querySelector('.addedItem__amount');
-    let pizzaAmount = Number(curentPizzaAmount.textContent);
-    const currentPizzaCost = document.querySelector('.addedItem__cost');
-    let pizzaCost = Number(currentPizzaCost.textContent);
-    pizzaPlus.forEach(element => {
-        element.addEventListener('click', () => {
-            curentPizzaAmount.textContent = ++pizzaAmount;
-            currentPizzaCost.textContent = Number(curentPizzaAmount.textContent) * pizzaCost;
-        });
-    });
-    pizzaMinus.forEach(element => {
-        element.addEventListener('click', () => {
-            curentPizzaAmount.textContent = --pizzaAmount;
-            currentPizzaCost.textContent = Number(curentPizzaAmount.textContent) * pizzaCost;
-        });
-    });
+<div class="addedItem">
+<span class="addedItem__remove">&times;</span>
+<div class="addedItem__img">${myPizza.img}</div>
+<span class="addedItem__name">${myPizza.name}</span>
+<img src="./img/minusbutton.png" alt="" class="addedItem__minusButton">
+<img src="./img/plusbutton.png" alt="" class="addedItem__plusButton">
+<span class="addedItem__amount">1</span>
+<span class="addedItem__cost">${myPizza.price}</span>
+</div>
+`)
+}
+function findPizza(pizzaId) {
+    const myPizza = pizzaData.find(item => item.id === pizzaId.id);
+    return addFoundPizza(myPizza)
 };
 const addBtn = Array.from(document.getElementsByClassName('card__button'));
 addBtn.forEach(element => {
     const elementParrent = element.closest('.menu__card');
     element.addEventListener("click", () => {
-        const dataIndex = elementParrent.getAttribute('data-id');
-        const test = pizzaData.find(item => item.id === Number(dataIndex));
-        if (1 == 2) {
-
-        }
-        addChosenPizza(test);
+        const dataIndex = Number(elementParrent.getAttribute('data-id'));
+        localStorage.setItem(`pizza${dataIndex}`, JSON.stringify({ id: dataIndex, count: 1 }))
+        const storageData = JSON.parse(localStorage.getItem(`pizza${dataIndex}`));
+        findPizza(storageData);
     })
 });
-
-/*toDo list  
-1) Сделать зависимость передачи данных из pizzaData в корзину в зависимости от того 
-какую пицу заказали. -done!
-2) Сделать проверку на наличие объекта в корзине что бы не добавлять одинаковые элементы
-3) Сделать закрытие корзины по клику вне ее окна
-4) Сделать счетчик заказа на корзине в header
-5) Сделать отображение цены в десятичных
-6) Убрать возможность делать отрицательное количество елемента в корзине
-7) узнать почему before не заданных размеров
-*/
-
-function name(params) {
-    
+// -------------delete added pizza---------------------------------------
+function deleteItem(item) {
+    item.parentElement.remove();
+};
+modal[0].addEventListener('click', function (e) {
+    if (e.target.closest('.addedItem__remove')) {
+        deleteItem(e.target.closest('.addedItem__remove'));
+    }
+});
+//-------------edit amount of pizza---------------------------------------
+function increaseAmount(item) {
+    const itemParrent = item.closest('.addedItem');
+    const currentPizzaAmount = itemParrent.querySelector('.addedItem__amount');
+    let pizzaAmount = Number(currentPizzaAmount.textContent);
+    const currentPizzaCost = itemParrent.querySelector('.addedItem__cost');
+    const pizzaCost = Number(currentPizzaCost.textContent) / pizzaAmount;
+    currentPizzaAmount.textContent = ++pizzaAmount;
+    currentPizzaCost.textContent = Number(currentPizzaAmount.textContent) * pizzaCost;
+};
+function decreaseamount(item) {
+    const itemParrent = item.closest('.addedItem');
+    const currentPizzaAmount = itemParrent.querySelector('.addedItem__amount');
+    let pizzaAmount = Number(currentPizzaAmount.textContent);
+    const currentPizzaCost = itemParrent.querySelector('.addedItem__cost');
+    const pizzaCost = Number(currentPizzaCost.textContent) / pizzaAmount;
+    if (pizzaAmount > 1) {
+        currentPizzaAmount.textContent = --pizzaAmount;
+        currentPizzaCost.textContent = Number(currentPizzaAmount.textContent) * pizzaCost;
+    }
 }
-[{id:1,
-count:10,}]
+modal[0].addEventListener('click', function (e) {
+    if (e.target.closest('.addedItem__plusButton')) {
+        increaseAmount(e.target.closest('.addedItem__plusButton'));
+    }
+});
+modal[0].addEventListener('click', function (e) {
+    if (e.target.closest('.addedItem__minusButton')) {
+        decreaseamount(e.target.closest('.addedItem__minusButton'));
+    }
+});
