773e8539a436fa16248c96abf9d04417a44875a1
Дима98678 <verhovod20077654@gmail.com>
2022-07-14 18:19:49 +0300

    final change in the file Pizza

--- Dmitry_tasks/js/Bank/script.js
+++ Dmitry_tasks/js/Bank/script.js
@@ -1,18 +1,10 @@
 'use strict';
 // Elements
-const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
 
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
-
-const deposit = document.querySelector('.movements__row');
-const withdrawal = document.querySelector('.movements__row-withdrawl');
 
 const btnLogin = document.querySelector('.login__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
@@ -27,11 +19,12 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-
+let today = new Date();
+const fiveMin = Number(new Date()) + 10000;
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [1300, 300],
-  interestRate: 1.2, // %
+  interestRate: 1.2,
   login: 'js',
   pin: 1,
 };
@@ -64,26 +57,53 @@ const accounts = [account1, account2, account3, account4];
 // Регистрация
 btnLogin.addEventListener('click', () => {
   const localAccounts = JSON.parse(localStorage.getItem('accounts'));
-  const activeUser= localAccounts.find(elem => elem.login === inputLoginUsername.value && elem.pin === Number(inputLoginPin.value))
-  if (activeUser !== undefined){
+  const activeUser = localAccounts.find(elem => elem.login === inputLoginUsername.value && elem.pin === Number(inputLoginPin.value))
+  if (activeUser.login !== undefined){
     inputLoginUsername.value = "";
     inputLoginPin.value = "";
     containerApp.style.opacity = "1";
+    labelDate.textContent = `${today.getDate()}/0${today.getMonth() + 1}/${today.getFullYear()}`;
     localStorage.setItem("activeUser", activeUser.login);
+    localStorage.setItem('fiveMin', JSON.stringify(fiveMin))
+    startTimer()
     movements()
-  } else alert("Неправильный логин или пароль!")
-})
-// console.log(JSON.parse(localStorage.getItem('accounts')));
-
-function init() {
-  if (localStorage.getItem('accounts')){
-    return
+  } else {
+    inputLoginUsername.value = "";
+    inputLoginPin.value = "";
+    alert("Неправильный логин или пароль!")
   }
-  else {
+})
+function init() {
+  if (!JSON.parse(localStorage.getItem('accounts'))) {
     localStorage.setItem('accounts', JSON.stringify(accounts));
+  }
+  if (localStorage.getItem("activeUser")){
+    inputLoginUsername.value = "";
+    inputLoginPin.value = "";
+    containerApp.style.opacity = "1";
+    labelDate.textContent = `${today.getDate()}/0${today.getMonth() + 1}/${today.getFullYear()}`;
+    startTimer();
+    movements()
   }
 }
 init()
+function startTimer() {
+  const start = setInterval(startTimer, 1000);
+  const now = Number(new Date())
+  const minutes1 = document.querySelector('.minutes');
+  const seconds1 = document.querySelector('.seconds');
+  const timer = JSON.parse(localStorage.getItem('fiveMin')) - now;
+  // console.log(timer);
+  if (timer <= 0) {
+    clearInterval(start);
+    containerApp.style.opacity = "0";
+  }
+  const minutes = timer > 0 ? Math.floor(timer / 1000 / 60) % 60 : 0;
+  const seconds = timer > 0 ? Math.floor(timer /1000) % 60 : 0;
+
+  minutes1.textContent = minutes < 10 ? '0' + minutes : minutes;
+  seconds1.textContent = seconds < 10 ? '0' + seconds : seconds;
+}
 // Денежные операции
 function movements(){
   const persons = JSON.parse(localStorage.getItem('accounts'));
@@ -94,16 +114,14 @@ function movements(){
       containerMovements.innerHTML +=
           `<div class="movements__row">
           <div class="movements__type movements__type--deposit">deposit</div>
-          <div class="movements__date">3 days ago</div>
+          <div class="movements__date">${today.getDate()}.0${today.getMonth() + 1}.${today.getFullYear()}, ${today.getHours()}:${today.getMinutes()}</div>
           <div class="movements__value">${elem}</div
         </div>`
     } else {
       containerMovements.innerHTML +=
           `<div class="movements__row-withdrawl">
-          <div class="movements__type movements__type--withdrawal">
-            1 withdrawal
-          </div>
-          <div class="movements__date">24/01/2037</div>
+          <div class="movements__type movements__type--withdrawal">withdrawal</div>
+          <div class="movements__date">${today.getDate()}.0${today.getMonth() + 1}.${today.getFullYear()}, ${today.getHours()}:${today.getMinutes()}</div>
           <div class="movements__value--minus">${elem}</div>
         </div>`
     }
@@ -165,20 +183,12 @@ btnSort.addEventListener('click', () =>{
 btnClose.addEventListener('click', () =>{
   const persons = JSON.parse(localStorage.getItem('accounts'))
   const neededPerson = persons.find(elem => elem.login === localStorage.getItem('activeUser'));
-  // console.log(persons.indexOf(neededPerson));
   if (inputCloseUsername.value === neededPerson.login && Number(inputClosePin.value) === neededPerson.pin){
     const index = persons.indexOf(neededPerson)
     persons.splice(index, 1);
     localStorage.setItem('accounts', JSON.stringify(persons))
+    localStorage.removeItem('activeUser');
+    containerApp.style.opacity = "0";
     init()
   }
 })
-
-
-
-
-
-
-
-
-
