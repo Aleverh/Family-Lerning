c394e8cccf22477ca11971f8d4b53cbffe3a7084
Дима98678 <verhovod20077654@gmail.com>
2022-09-02 12:19:29 +0300

    Project is ready

---
+++ Dmitry_tasks/js/Recipes/src/js/constants.js
@@ -0,0 +1,5 @@
+export const recipeContainer = document.querySelector('.recipe');
+export const allRecipes = document.querySelector('.results');
+export const bookmarks = document.querySelector('.bookmarks');
+export const bookmarksList = document.querySelector('.bookmarks__list');
+
